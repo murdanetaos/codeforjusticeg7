7c120336706fbbb6ab9c4baf36b703a71dc23841
Kevin Lam <Kevin@Administrators-MacBook-Pro.local>
2023-04-11 16:22:28 -0400

    updated main page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const navbar = document.getElementById("navbar");
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li
+// const navbar = document.getElementById("navbar");
+// const navSlide = () => {
+//   const burger = document.querySelector(".burger");
+//   const nav = document.querySelector(".nav-links");
+//   const navLinks = document.querySelectorAll(".nav-links li");
+// };
